1a1d61b71de8e54af91a37e9213051883f6795cb
knifour <knifour@hotmail.com.tw>
2019-12-30 16:15:53 +0800

    terminal window建構函式修改

--- include/twindow.h
+++ include/twindow.h
@@ -1,7 +1,7 @@
 /* 類別名稱TWINDOW表示為TERMINAL WINDOW之意，用以跟圖形WINDOW區別 */
 /* 它是所有視窗相關類別的父類別 */
 
-ifndef __TWINDOW_H
+#ifndef __TWINDOW_H
 #define __TWINDOW_H
 
 #include <string>
@@ -15,11 +15,11 @@ using namespace std;
 /* 先後順序主要在定義重繪時的先後順序(依照視窗建立順序) */
 /* SCREEN的mTWincnt變數儲存TWINDOW類別總數 */
 typedef struct {
-	SCREEN *parant;    /* 儲存父視窗指標 */
-	int lin, col;      /* 視窗左上角座標 */
-	int LINS, COLS;    /* 視窗列數、行數 */
-	int FColor, BColor /* 視窗前景、背景色 */
-	bool Borad;        /* 是否有外框，有外框時視窗列數必須大於等於3，行數必須大於等於6 */
+	SCREEN *parant;     /* 儲存父視窗指標 */
+	int lin, col;       /* 視窗左上角座標 */
+	int LINS, COLS;     /* 視窗列數、行數 */
+	int FColor, BColor; /* 視窗前景、背景色 */
+	bool Board;         /* 是否有外框，有外框時視窗列數必須大於等於3，行數必須大於等於6 */
 } TWS;
 
 class TWINDOW{
@@ -27,12 +27,12 @@ public:
   TWINDOW(TWS);            /* 視窗建構函式 */
 	~TWINDOW();
 
-private:
+protected:
 	TWS tws;                 /* 視窗基本資料結構 */
-	bool mError;              /* 此值若為true，表示視窗的設定值嚴重錯誤，任何對視窗的操作都沒有作用 */
-	bool mShow;              /* 視窗是否顯示，預設為不顯示 */
-	UTF8SCHAR **TWindowBuf;  /* 視窗緩衝區 */
+	bool mError;             /* 此值若為true，表示視窗的設定值嚴重錯誤，任何對視窗的操作都沒有作用 */
+	bool mVisible;           /* 視窗是否顯示，預設為不顯示 */
+	UTF8SCHAR **TWinBuf;     /* 視窗緩衝區 */
 	SCHAR* sp;               /* 視窗字元操作指標 */
-}
+};
 
 #endif
--- twindow/twindow.cpp
+++ twindow/twindow.cpp
@@ -3,9 +3,61 @@
 #include <twindow.h>
 
 TWINDOW::TWINDOW(TWS p){
-	if (p.parant == NULL){  // 若沒有設定父視窗指標
+	int ln, cl;
+	
+	tws = p;
+	
+	if (tws.parant == NULL){  // 若沒有設定父視窗指標
+		mError = true;  // 將視窗設定為不能使用狀態
+		return;
+	}
+	
+	ln = tws.parant->GetLins();
+	cl = tws.parant->GetCols();
+	if (tws.lin < 1 || tws.lin > ln){  // 如果視窗左上角列座標超過螢幕範圍
 		mError = true;
 		return;
 	}
 	
+	if (tws.col < 1 || tws.col > cl){  // 如果視窗左上角行座標超過螢幕範圍
+		mError = true;
+		return;
+	}
+	
+	if ((tws.lin+tws.LINS-1) > ln){  // 如果視窗右下角列座標超過螢幕範圍
+		mError = true;
+		return;
+	}
+	
+	if ((tws.col+tws.COLS-1) > cl){  // 如果視窗右下角行座標超過螢幕範圍
+		mError = true;
+		return;
+	}
+	
+	if ((tws.Board && tws.LINS < 3) || (tws.Board && tws.COLS < 6)){  // 如果有設定外框且列數少於3或行數少於6
+		mError = true;
+		return;
+	}
+	
+	if (tws.FColor < 0 || tws.FColor > 255) // 如果前景色超出範圍
+		tws.FColor = 7;
+	
+	if (tws.BColor < 0 || tws.BColor > 255) // 如果背景色超出範圍
+		tws.BColor = 0;
+		
+	mError = false;
+	mVisible = false;
+	
+	TWinBuf = new UTF8SCHAR*[tws.LINS];  // 依據視窗總列數建立UTF8SCHAR指標陣列
+	for (int i=0; i<tws.LINS; i++){
+		/* 依據視窗每列字數建立UTF8SCHAR物件陣列 */
+		TWinBuf[i] = new UTF8SCHAR[tws.COLS];  
+	}
+}
+
+TWINDOW::~TWINDOW(){
+	for (int i=0; i<tws.LINS; i++)
+		delete[] TWinBuf[i];
+	
+	delete[] TWinBuf;
 }
