33239e30d89aea6c3ece95ec2455d38132d808f2
knifour <knifour@hotmail.com.tw>
2020-01-03 15:52:14 +0800

    測試類別可否使用不定長度引數

--- exam/args.cpp
+++ exam/args.cpp
@@ -3,43 +3,76 @@
 
 using namespace std;
 
-/* l = lin, 
-   c = col, 
-	 L = LINES, 
-	 C = COLUMNS */
-void para(const char *fmt, ...);
+class Win{
+public:
+  Win(const char *fmt, ...);
+	void print(void);
 
-int main(void){
-	
-	para("clC", 7, 31, 28);
-	
-  return 0;
-}
+private:
+  int mLin, mCol;
+  int mLINS, mCOLS;	
+};
 
-void para(const char *fmt, ...){
+Win::Win(const char *fmt, ...){
 	va_list ap;
 	int result;
+	
+	mLin = 1;
+	mCol = 1;
+	mLINS = 3;
+	mCOLS = 6;
 	
 	va_start(ap, fmt);
 	while (*fmt){
 		switch (*fmt++){
 		case 'l':
 		  result = va_arg(ap, int);
-			printf("line=%d\n", result);
+			if (result < 1)
+				result = 1;
+			if (result > 24)
+				result = 24;
+			mLin = result;
 			break;
 		case 'c':
 		  result = va_arg(ap, int);
-			printf("column=%d\n", result);
+			if (result < 1)
+				result = 1;
+			if (result > 80)
+				result = 80;
+			mCol = result;
 			break;
 		case 'L':
 		  result = va_arg(ap, int);
-			printf("Lines=%d\n", result);
+			if (result < 1)
+				result = 1;
+			if (result > 30)
+				result = 30;
+		  mLINS = result;
 			break;
 		case 'C':
 		  result = va_arg(ap, int);
-			printf("Columns=%d\n", result);
-			break;
+			if (result < 1)
+				result = 1;
+			if (result > 100)
+				result = 100;
+			mCOLS = result;
+			break;	
 		}
 	}
 	va_end(ap);
+}
+
+void Win::print(void){
+	printf("lin = %d\n", mLin);
+	printf("column = %d\n", mCol);
+	printf("Lines = %d\n", mLINS);
+	printf("Columns = %d\n", mCOLS);
+}
+
+int main(void){
+	Win aw("lcLC", 10, 21, 10, 20);
+	
+	aw.print();
+	
+  return 0;
 }
