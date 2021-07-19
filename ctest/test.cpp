100e0629eca2568e9c2590b91301d7a4cb066f76
knifour <knifour@hotmail.com.tw>
2021-07-19 09:36:09 +0800

    classtest

--- ctest/test.cpp
+++ ctest/test.cpp
@@ -6,6 +6,12 @@ using namespace std;
 int main(void){
 	FATHER *p1 = new SON();
 	FATHER *p2 = new SON();
+	FATHER *p3 = new SON();
+	
+	if (*p1 == *p2)
+		cout << "The same" << endl;
+	else
+		cout << "Not equal" << endl;
 	
 	p2->setSon(8);
 	
@@ -14,8 +20,16 @@ int main(void){
 	else
 		cout << "Not equal" << endl;
 	
+	*p3 = *p2;
+	
+	if (*p2 == *p3)
+		cout << "The same" << endl;
+	else
+		cout << "Not equal" << endl;
+	
 	p1->printMember();
 	p2->printMember();
+	p3->printMember();
 	
 	return 0;
 }
