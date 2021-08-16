50d3541e306ff042a34a1361eab1ba928b1c38b4
knifour <knifour@hotmail.com.tw>
2021-08-16 11:01:58 +0800

    bug fixed

--- dtime/test.cpp
+++ dtime/test.cpp
@@ -1,4 +1,5 @@
 #include <date.h>
+#include <ltime.h>
 #include <iostream>
 
 void delay(int);
@@ -8,9 +9,11 @@ using namespace std;
 int main(void){
 	string dname[7] = {"日", "一", "二", "三", "四", "五", "六"};
 	DATE now;
+	LTIME t1, t2(16, 30, 0);
 
   cout << "中華民國" << now.toString(MINGSLASH);
 	cout << "星期" << dname[now.getDayOfWeek()] << endl;
+	cout << (t2-t1)/60 << "分" << (t2-t1)%60 << "秒" << endl;
 	
 	return 0;
 }
