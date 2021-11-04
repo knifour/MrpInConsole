99b9339bd042e6aeef398e832de476642dc5c457
knifour <knifour@hotmail.com.tw>
2021-11-04 17:08:30 +0800

    exam program

--- exam/test.cpp
+++ exam/test.cpp
@@ -1,4 +1,32 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termcap.h>
+
+int main(){
+	char buf[1024];
+	char buf2[30];
+	char *ap = buf2;
+	char *clearstr, *gotostr, *standstr, *stendstr;
+	
+	tgetent(buf, getenv("TERM"));
+	
+	clearstr = tgetstr("cl", &ap);
+	gotostr = tgetstr("cm", &ap);
+	standstr = tgetstr("so", &ap);
+	stendstr = tgetstr("se", &ap);
+	
+	fputs(clearstr, stdout);
+	fputs(tgoto(gotostr, 20, 10), stdout);
+	printf("您好, ");
+	fputs(standstr, stdout);
+	printf("世界");
+	fputs(stendstr, stdout);
+	putchar('!');
+	
+	return 0;
+}
+
+/*#include <iostream>
 #include <fstream>
 #include <iconv.h>
 #include <stdio.h>
@@ -88,5 +116,5 @@ int main(void){
 	}
 	
 	src.close();
-	return 0;*/
-}
+	return 0;
+}*/
